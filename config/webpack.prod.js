720193d2380133aef3027ec69654f0ac4ff06f45
MacTavish Lee <1064416560@qq.com>
2019-05-26 16:51:07 +0800

    :zap: feat: 使用ParallelUglifyPlugin 优化代码压缩

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const DllReferencePlugin = require('webpack/lib/DllReferencePlugin');
 const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 const prodConfig = {
   mode: 'production',
@@ -16,6 +18,29 @@ const prodConfig = {
     }),
     new AddAssetHtmlWebpackPlugin({
       filepath: path.resolve(__dirname, '../dll', 'react.dll.js')
+    }),
+    // 使用 ParallelUglifyPlugin 并行压缩输出的 JS
+    new ParallelUglifyPlugin({
+      exclude: /node_modules/,
+      // UglifyJS 的参数
+      uglifyJS: {
+        output: {
+          // 紧凑输出
+          beautify: false,
+          // 去除注释
+          comments: false
+        },
+        compress: {
+          // 删除无用代码时不产生警告
+          warnings: false,
+          // 去除所有的 console
+          drop_console: true,
+          // 内嵌定义但只用到一次的变量
+          collapse_vars: true,
+          // 提取出使用多次但没有定义为变量的静态值
+          reduce_vars: true
+        }
+      }
     })
   ]
 }
